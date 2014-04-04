3681bbf194024db3e6b1ec104aa5714ff5a3f286
esemi <spam@esemi.ru>
2014-04-04 16:51:48 +0400

    fix unvalid image

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 
 __author__ = 'esemi'
 
+import timeit
+
 
 def test_imghdr(filename):
     import imghdr
+
     try:
         fd = open(filename, 'rb').read()
         return (imghdr.what(None, fd) == 'jpeg')
@@ -27,14 +30,13 @@ def test_pil(filename):
 
 if __name__ == '__main__':
 
-    print test_imghdr('test.jpg') == True
-    print test_imghdr('unvalid.jpg') == False
-    print test_imghdr('notfound.jpg') == False
+    assert test_imghdr('test.jpg') == True
+    assert test_imghdr('unvalid.jpg') == False
+    assert test_imghdr('notfound.jpg') == False
 
-    print test_pil('test.jpg') == True
-    print test_pil('unvalid.jpg') == False
-    print test_pil('notfound.jpg') == False
+    assert test_pil('test.jpg') == True
+    assert test_pil('unvalid.jpg') == False
+    assert test_pil('notfound.jpg') == False
 
-    import timeit
     print "test_imghdr %.2f" % timeit.timeit("test_imghdr('test.jpg')", setup="from __main__ import test_imghdr")
     print "test_pil %.2f" %  timeit.timeit("test_pil('test.jpg')", setup="from __main__ import test_pil")
